b89e347eb2df13a89ce106d9904ecdab93802259
Matthew Weigel <unique@idempot.net>
2020-12-03 22:58:31 -0600

    First day's solutions.

---
+++ day-1.cc
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <list>
+
+int main(int argc, char *argv[]) {
+  FILE *input = fopen("input", "r");
+  if (!input) return 1;
+
+  std::list<int64_t> expenses;
+  int64_t expense;
+
+  while (fscanf(input, "%lld", &expense) > 0)
+    expenses.push_back(expense);
+
+  for (std::list<int64_t>::iterator it = expenses.begin(); it != expenses.end(); ++it) {
+    std::list<int64_t>::iterator jt = it;
+    ++jt;
+    for (; jt != expenses.end(); ++jt) {
+      std::list<int64_t>::iterator kt = jt;
+      ++kt;
+      for (; kt != expenses.end(); ++kt) {
+        if (*it + *jt + *kt == 2020) {
+          printf("result is %lld\n", *it * *jt * *kt);
+          return 0;
+        }
+      }
+    }
+  }
+  printf("No result found.\n");
+
+  return 0;
+}
