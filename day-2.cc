8855e558839a16be822ee5e52c84dfd0c5bbd5ea
Matthew Weigel <unique@idempot.net>
2020-12-03 23:23:13 -0600

    Second step of day 2 solved.

--- day-2.cc
+++ day-2.cc
@@ -12,10 +12,17 @@ int main(int argc, char *argv[]) {
   while (fscanf(input, "%d-%d %c: %s", &min, &max, &req_char, password) == 4) {
     int count = 0;
     int len = strnlen(password, 128);
-    for (int i = 0; i < len; ++i) {
-      if (password[i] == req_char) count++;
-    }
-    if (count >= min && count <= max) {
+    bool just_one = false;
+    if (min <= len)
+      if (password[min - 1] == req_char)
+        just_one = !just_one;
+    if (max <= len)
+      if (password[max - 1] == req_char)
+        just_one = !just_one;
+
+    if (just_one) {
+      printf("%d-%d %c: %s - ", min, max, req_char, password);
+      printf("%c, %c\n", password[min - 1], password[max - 1]);
       valid++;
     }
   }
