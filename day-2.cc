8f4fcc4a809b86ae4e97012e2a706219978dc0fe
Matthew Weigel <unique@idempot.net>
2020-12-05 15:30:24 -0600

    One program to solve part 1 and 2 of day 2.

--- day-2.cc
+++ day-2.cc
@@ -5,13 +5,21 @@ int main(int argc, char *argv[]) {
   FILE *input = fopen("day-2-input.txt", "r");
   if (!input) return 1;
 
-  int valid = 0;
+  int part_1_valid = 0;
+  int part_2_valid = 0;
   int min, max;
   char req_char;
   char password[129];
   while (fscanf(input, "%d-%d %c: %s", &min, &max, &req_char, password) == 4) {
     int count = 0;
     int len = strnlen(password, 128);
+    for (int i = 0; i < len; ++i) {
+      if (password[i] == req_char)
+        count++;
+    }
+    if (count >= min && count <= max) {
+      part_1_valid++;
+    }
     bool just_one = false;
     if (min <= len)
       if (password[min - 1] == req_char)
@@ -23,11 +31,12 @@ int main(int argc, char *argv[]) {
     if (just_one) {
       printf("%d-%d %c: %s - ", min, max, req_char, password);
       printf("%c, %c\n", password[min - 1], password[max - 1]);
-      valid++;
+      part_2_valid++;
     }
   }
 
-  printf("Valid count is %d\n", valid);
+  printf("Part 1 valid count is %d\n", part_1_valid);
+  printf("Part 2 valid count is %d\n", part_2_valid);
 
   return 0;
 }
