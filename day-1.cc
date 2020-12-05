797889c1dd3fd53ec21c2d9f1f890dba5bb68b86
Matthew Weigel <unique@idempot.net>
2020-12-05 15:26:04 -0600

    One program to solve part 1 and 2 of day 1.

--- day-1.cc
+++ day-1.cc
@@ -11,17 +11,26 @@ int main(int argc, char *argv[]) {
   while (fscanf(input, "%lld", &expense) > 0)
     expenses.push_back(expense);
 
+  bool part_1 = false;
+  bool part_2 = false;
+
   for (std::list<int64_t>::iterator it = expenses.begin(); it != expenses.end(); ++it) {
     std::list<int64_t>::iterator jt = it;
     ++jt;
     for (; jt != expenses.end(); ++jt) {
+      if (!part_1 && *it + *jt == 2020) {
+        part_1 = true;
+        printf("part 1 answer is %lld\n", *it * *jt);
+      }
       std::list<int64_t>::iterator kt = jt;
       ++kt;
       for (; kt != expenses.end(); ++kt) {
-        if (*it + *jt + *kt == 2020) {
+        if (!part_2 && *it + *jt + *kt == 2020) {
           printf("result is %lld\n", *it * *jt * *kt);
-          return 0;
+          part_2 = true;
         }
+        if (part_1 && part_2)
+          return 0;
       }
     }
   }
