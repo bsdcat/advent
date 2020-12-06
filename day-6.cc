03a63e3499d06d5ecdcf00a16d731a7061ef93b3
Matthew Weigel <unique@idempot.net>
2020-12-06 09:09:20 -0600

    Second step of day 6 solved.

--- day-6.cc
+++ day-6.cc
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <fstream>
+#include <map>
 #include <set>
 
 int main(int argc, char *argv[]) {
@@ -8,30 +9,47 @@ int main(int argc, char *argv[]) {
     return 1;
 
   char line[256] = {0};
-  int sum = 0;
+  int part_1 = 0;
+  int part_2 = 0;
   std::set<char> answers;
 
+  std::map<char, int> answer_count;
+  int group_size = 0;
+  bool new_group = true;
+
   while (input.getline(line, 256)) {
-    //printf("line is %s\n", line);
     int length = input.gcount();
-    //printf("length is %\n", length);
     if (length > 1) {
-      printf("%s", line);
-      for (int i = 0; i < length - 1; ++i) {
-        //printf("%c", line[i]);
-        answers.insert(line[i]);
+      if (new_group) {
+        group_size = 0;
+        new_group = false;
       }
-      printf(" %lu\n", answers.size());
-      printf("\n");
+      group_size++;
+      for (int i = 0; i < length - 1; ++i) {
+        answers.insert(line[i]);
+        answer_count[line[i]]++;
+      }
     } else {
-      // printf("got here\n");
-      sum += answers.size();
+      part_1 += answers.size();
+      for (auto it = answer_count.begin(); it != answer_count.end(); ++it) {
+        if (it->second == group_size) {
+          part_2++;
+        }
+      }
       answers.clear();
+      answer_count.clear();
+      new_group = true;
     }
   }
-  sum += answers.size();
+  part_1 += answers.size();
+  for (auto it = answer_count.begin(); it != answer_count.end(); ++it) {
+    if (it->second == group_size) {
+      part_2++;
+    }
+  }
 
-  printf("Sum for part 1 is %d\n", sum);
+  printf("Sum for part 1 is %d\n", part_1);
+  printf("Sum for part 2 is %d\n", part_2);
   
   return 0;
 }
