612fd3c0cce7e0e04e2cc117681e78b2073b8ab1
Matthew Weigel <unique@idempot.net>
2020-12-07 00:10:55 -0600

    Second step of day 7 solved.

--- 2020/day-7.cc
+++ 2020/day-7.cc
@@ -10,8 +10,26 @@ struct bag {
   std::map<std::string, int> contains;
 };
 
+uint64_t count_bags(const std::string &name, std::map<std::string, bag *> &bag_map) {
+  static std::map<std::string, uint64_t> seen_bags;
+  auto it = seen_bags.find(name);
+  if (it != seen_bags.end()) {
+    return it->second;
+  }
+
+  printf("checking for bags I need in a '%s' bag\n", name.c_str());
+  bag *this_bag = bag_map[name];
+  uint64_t accumulator = 0;
+  for (const std::pair<std::string, int> &container : this_bag->contains) {
+    accumulator += container.second;
+    accumulator += container.second * count_bags(container.first, bag_map);
+  }
+  seen_bags[name] = accumulator;
+  return accumulator;
+}
+
 int main(int argc, char *argv[]) {
-  std::ifstream input = std::ifstream("day-7-test-input.txt");
+  std::ifstream input = std::ifstream("day-7-input.txt");
   if (!input.is_open())
     return 1;
   
@@ -25,7 +43,7 @@ int main(int argc, char *argv[]) {
     bag * new_bag = new bag();
     new_bag->name = name;
     bag_map[name] = new_bag;
-    printf("bag type is %s\n", name.c_str());
+    //  printf("bag type is %s\n", name.c_str());
     offset += 14;
     while (offset != std::string::npos) {
       int count = 0;
@@ -35,7 +53,7 @@ int main(int argc, char *argv[]) {
         size_t bag_offset = linestr.find(" bag", after_num_offset);
         std::string contains = linestr.substr(after_num_offset + 1, bag_offset - after_num_offset - 1);
         new_bag->contains[contains] = count;
-        printf("\t%d '%s'\n", count, contains.c_str());
+        //  printf("\t%d '%s'\n", count, contains.c_str());
         offset = linestr.find_first_of("123456789", after_num_offset);
       } else {
         offset = std::string::npos;
@@ -60,6 +78,8 @@ int main(int argc, char *argv[]) {
   }
 
   printf("%lu different bags hold mine.\n", holds_mine.size());
-  
+
+  printf("%llu total bags required.\n", count_bags("shiny gold", bag_map));
+
   return 0;
 }
