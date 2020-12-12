879b36d3c2827e26cc2d4bf384322f80bf29df54
Matthew Weigel <unique@idempot.net>
2020-12-12 16:29:06 -0600

    Second step of day 10 solved.

--- 2020/day-10.cc
+++ 2020/day-10.cc
@@ -1,9 +1,29 @@
 #include <algorithm>
 #include <cstdio>
 #include <fstream>
-#include <list>
+#include <set>
 #include <string>
 #include <vector>
+
+int64_t count_perms(std::vector<int64_t> &adapters, std::vector<int64_t> &seen, int cur_index) {
+  if (cur_index == adapters.size() - 1)
+    return 1;
+
+  if (cur_index >= 0 && seen[cur_index] >= 0)
+    return seen[cur_index];
+
+  int64_t sum = 0;
+  int cur_jolts = 0;
+  if (cur_index >= 0)
+    cur_jolts = adapters[cur_index];
+  for (int i = cur_index + 1; i < cur_index + 4 && i < adapters.size(); ++i) {
+    if (adapters[i] > cur_jolts + 3)
+      break;
+    sum += count_perms(adapters, seen, i);
+  }
+  seen[cur_index] = sum;
+  return sum;
+}
 
 int main(int argc, char *argv[]) {
   std::ifstream input = std::ifstream("day-10-input.txt");
@@ -34,6 +54,9 @@ int main(int argc, char *argv[]) {
 
   printf("There are %d one-jolt differences and %d three-jolt differences, producing %d\n",
          one_jolt, three_jolt, one_jolt * three_jolt);
+
+  std::vector<int64_t> seen(adapters.size(), -1LL);
+  printf("There are %lld permutations of adapters\n", count_perms(adapters, seen, -1));
 
   return 0;
 }
