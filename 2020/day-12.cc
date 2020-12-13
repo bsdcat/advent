196cb2d43a0d436d119397f0f5fc646bc62b0266
Matthew Weigel <unique@idempot.net>
2020-12-12 18:51:25 -0600

    Second step of day 12 solved.

--- 2020/day-12.cc
+++ 2020/day-12.cc
@@ -18,7 +18,23 @@ enum heading_change {
   R = 1,
   L = 3, // -1 modulo 4
 };
-  
+
+void rotate_right(int &x, int &y) {
+  int new_x = y;
+  y = -x;
+  x = new_x;
+}
+
+void rotate_left(int &x, int &y) {
+  int new_y = x;
+  x = -y;
+  y = new_y;
+}
+
+void flip_back(int &x, int &y) {
+  x = -x;
+  y = -y;
+}
 
 int main(int argc, char *argv[]) {
   std::ifstream input = std::ifstream("day-12-input.txt");
@@ -82,6 +98,71 @@ int main(int argc, char *argv[]) {
 
   printf("result is %u east/west and %u north/south\n", abs(x), abs(y));
   printf("Manhattan distance is %u\n", abs(x) + abs(y));
+
+  x = 0;
+  y = 0;
+  int wx = 10;
+  int wy = 1;
+
+  for (std::string &line : instructions) {
+    int val = std::stoi(line.substr(1));
+    switch (line[0]) {
+    case 'F':
+      x += val * wx;
+      y += val * wy;
+      break;
+    case 'E':
+      wx += val;
+      break;
+    case 'W':
+      wx -= val;
+      break;
+    case 'N':
+      wy += val;
+      break;
+    case 'S':
+      wy -= val;
+      break;
+    case 'L':
+      switch (val) {
+      case 90:
+        rotate_left(wx, wy);
+        break;
+      case 180:
+        flip_back(wx, wy);
+        break;
+      case 270:
+        rotate_right(wx, wy);
+        break;
+      default:
+        printf("shoot\n");
+        exit(5);
+      }
+      break;
+    case 'R':
+      switch (val) {
+      case 90:
+        rotate_right(wx, wy);
+        break;
+      case 180:
+        flip_back(wx, wy);
+        break;
+      case 270:
+        rotate_left(wx, wy);
+        break;
+      default:
+        printf("oof\n");
+        exit(4);
+      }
+      break;
+    default:
+      printf("dang %c\n", line[0]);
+      exit(3);
+    }
+  }
+
+  printf("REAL result is %u east/west and %u north/south\n", abs(x), abs(y));
+  printf("REAL Manhattan distance is %u\n", abs(x) + abs(y));
 
   return 0;
 }
